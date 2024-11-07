5b29b01ee3aca48ddecd4deb7375a329a59b30b1
hendisantika <hendisantika@yahoo.co.id>
2024-11-07 09:06:02 +0700

    Add Test Containers🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/digitaloceanspace/SpringBootDigitalOceanSpaceApplicationTests.java
+++ src/test/java/id/my/hendisantika/digitaloceanspace/SpringBootDigitalOceanSpaceApplicationTests.java
@@ -1,13 +1,29 @@
 package id.my.hendisantika.digitaloceanspace;
 
-import org.junit.jupiter.api.Test;
+import id.my.hendisantika.digitaloceanspace.repository.ImageRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@Testcontainers
+@SpringBootTest(
+        properties = {
+                "management.endpoint.health.show-details=always",
+                "spring.datasource.url=jdbc:tc:postgresql:17-alpine3.20:///imageDB"
+        },
+        webEnvironment = RANDOM_PORT
+)
 class SpringBootDigitalOceanSpaceApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private ImageRepository imageRepository;
+
+    @BeforeEach
+    void deleteAll() {
+        imageRepository.deleteAll();
     }
 
 }
