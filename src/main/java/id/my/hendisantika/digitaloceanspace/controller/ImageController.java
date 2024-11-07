32d345c1a6edea1eeea6e4de122a85eda8b46a20
hendisantika <hendisantika@yahoo.co.id>
2024-11-07 08:08:48 +0700

    Add uploadFile in ImageController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/digitaloceanspace/controller/ImageController.java
+++ src/main/java/id/my/hendisantika/digitaloceanspace/controller/ImageController.java
@@ -1,9 +1,19 @@
 package id.my.hendisantika.digitaloceanspace.controller;
 
+import id.my.hendisantika.digitaloceanspace.model.Image;
 import id.my.hendisantika.digitaloceanspace.service.ImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +30,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/upload")
 public class ImageController {
     private final ImageService imageService;
+
+    @PostMapping(value = "/new",
+            consumes = MULTIPART_FORM_DATA_VALUE,
+            produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<Image> uploadFile(@RequestParam("file") final MultipartFile file) throws IOException {
+        var response = imageService.saveFile(file);
+        return ResponseEntity.ok(response);
+
+    }
 }
