d10d2f776f67b0de6063dba503a50653339abca0
hendisantika <hendisantika@yahoo.co.id>
2024-11-07 08:00:45 +0700

    Create Spring Boot App 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/digitaloceanspace/SpringBootDigitalOceanSpaceApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.digitaloceanspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDigitalOceanSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDigitalOceanSpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/digitaloceanspace/SpringBootDigitalOceanSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.digitaloceanspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDigitalOceanSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
